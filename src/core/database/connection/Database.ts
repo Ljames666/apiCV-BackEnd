60e97b571fb0c6c06359ec87436cb5af86cc3974
Jamerson Paz <ljames666@gmail.com>
2021-12-21 02:08:09 -0300

    okay

--- src/core/database/connection/Database.ts
+++ src/core/database/connection/Database.ts
@@ -1,13 +1,27 @@
 import { createConnection, getConnection as getConnectionTypeORM } from "typeorm";
 
-export const initConnection = async () => createConnection();
+export const initConnection = async () =>
+  await createConnection({
+    type: "postgres",
+    host: "ec2-34-198-223-125.compute-1.amazonaws.com",
+    port: 5432,
+    username: "hrixcsbjefrpfk",
+    password: "7029940c6a4e130de6442393f5523061af559f36f4b9f11e65625af861a84b5f",
+    database: "d57dr2p40a3jtd",
+    schema: "public",
+    entities: ["src/core/models/*.ts"],
+    migrations: ["src/core/database/migrations/*.ts"],
 
-export const getConnection = () => {
-  let conn = getConnectionTypeORM();
-
-  if (!conn) {
-    throw new Error("Database is not connected");
-  }
-
-  return conn;
-};
+    cli: {
+      entitiesDir: "src/core/models",
+      migrationsDir: "src/core/database/migrations",
+    },
+    synchronize: false,
+    logging: true,
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  });
