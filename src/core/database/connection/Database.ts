01cfcc65dce1981de1b7ffd26ed38b6cc9613b3f
Jamerson Paz <ljames666@gmail.com>
2021-12-22 18:51:49 -0300

    tenativa

--- src/core/database/connection/Database.ts
+++ src/core/database/connection/Database.ts
@@ -1,3 +1,27 @@
 import { createConnection } from "typeorm";
 
-export const initConnection = async () => await createConnection();
+export const initConnection = async () =>
+  await createConnection({
+    type: "postgres",
+    host: "ec2-34-198-223-125.compute-1.amazonaws.com",
+    port: 5432,
+    username: "hrixcsbjefrpfk",
+    password: "7029940c6a4e130de6442393f5523061af559f36f4b9f11e65625af861a84b5f",
+    database: "d57dr2p40a3jtd",
+    schema: "public",
+    entities: ["src/core/models/*.ts"],
+    migrations: ["src/core/database/migrations/*.ts"],
+
+    cli: {
+      entitiesDir: "src/core/models",
+      migrationsDir: "src/core/database/migrations",
+    },
+    synchronize: false,
+    logging: true,
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  });
